ba5cfc92711759c4979719d5f884398387a1afab
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-03-04 15:21:09 +0100

    filtr-menu-js

--- js/filtr-menu.js
+++ js/filtr-menu.js
@@ -1,6 +1,11 @@
-const filtrButtonsContainer = document.querySelector('ul.projects-filtr');
-const filtrAllCards = [...document.querySelectorAll('ul.our-projects li')];
-filtrButtonsContainer.addEventListener('click', filtrBtnClickHandler);
+import { portfolioItems } from './portfolio-iitems.js';
+
+const refs = {
+  filtrButtonsContainer: document.querySelector('ul.projects-filtr'),
+  portfolioGalleryContainer: document.querySelector('ul.our-projects'),
+};
+
+refs.filtrButtonsContainer.addEventListener('click', filtrBtnClickHandler);
 
 function filtrBtnClickHandler(evt) {
   if (evt.target.nodeName !== 'BUTTON') {
@@ -10,27 +15,44 @@ function filtrBtnClickHandler(evt) {
   const previousActiveBtn = document.querySelector('.projects-filtr__button--active');
   previousActiveBtn.classList.remove('projects-filtr__button--active');
   const currentActiveBtn = evt.target;
-
   currentActiveBtn.classList.add('projects-filtr__button--active');
 
-  cleanCardsFilter();
-  filtredCards();
-
-  function filtredCards() {
-    if (currentActiveBtn.dataset.value === 'all') {
-      return cleanCardsFilter();
-    }
-    filtrAllCards.reduce((previousValue, element) => {
-      if (currentActiveBtn.dataset.value === element.dataset.value) {
-        return element;
-      }
-      return element.classList.add('visually-hidden');
-    }, 0);
+  refs.portfolioGalleryContainer.innerHTML = '';
+  if (evt.target.dataset.value === 'all') {
+    refs.portfolioGalleryContainer.innerHTML = createPortfolioGalleyMarkup(portfolioItems);
+    return;
   }
+  refs.portfolioGalleryContainer.innerHTML = createPortfolioGalleyMarkup(
+    portfolioItems.filter(item => item.dataValue === evt.target.dataset.value)
+  );
+}
 
-  function cleanCardsFilter() {
-    filtrAllCards.reduce((previousValue, element) => {
-      return element.classList.remove('visually-hidden');
-    }, 0);
-  }
+function createPortfolioGalleyMarkup(portfolioItems) {
+  return portfolioItems
+    .map(({ dataValue, image, overlayText, itemTitle, itemText }) => {
+      return `<li class="our-projects__item" data-value="${dataValue}">
+          <a class="our-projects__link" href="#" target="_blank" rel="noopener noreferrer nofollow">
+            <div class="our-projects__overlay-borders">
+              <img
+                srcset="
+                     ${image.srcset}
+                    "
+                sizes="(min-width: 1200px) 370px, (min-width: 768px) 354px, (min-width: 480px) 450px, (min-width: 320px) 100vw"
+                src="${image.src}"
+                alt="${image.alt}"
+              />
+
+              <p class="our-projects__overlay">
+                ${overlayText}
+              </p>
+            </div>
+
+            <div class="our-projects__thumb">
+              <h2 class="our-projects__title">${itemTitle}</h2>
+              <p class="our-projects__text">${itemText}</p>
+            </div>
+          </a>
+        </li>`;
+    })
+    .join('');
 }
---
+++ js/portfolio-iitems.js
@@ -0,0 +1,128 @@
+export const portfolioItems = [
+  {
+    dataValue: 'web',
+    image: {
+      srcset:
+        './images/our-projects/technocrack_phone_450.jpg 450w, ./images/our-projects/technocrack_phone_450x2.jpg 900w, ./images/our-projects/technocrack_tablet_354.jpg 354w,./images/our-projects/technocrack_tablet_354x2.jpg  708w, ./images/our-projects/technocrack_desktop_370.jpg 370w, ./images/our-projects/technocrack_desktop_370x2.jpg 740w',
+      src: './images/our-projects/technocrack_tablet_354.jpg',
+      alt: 'відкрита сторінка сайту',
+    },
+    overlayText:
+      'Ресурс пропонує комплексні пропозиції з різним рівнем функціоналу та сервісів. Все це дозволить відвідувачу отримати вичерпні відомості про компанію чи                   приватну особу.',
+    itemTitle: 'Технокряк',
+    itemText: 'Веб-сайт',
+  },
+
+  {
+    dataValue: 'design',
+    image: {
+      srcset:
+        './images/our-projects/neworlean_phone_450.jpg 450w, ./images/our-projects/neworlean_phone_450x2.jpg 900w, ./images/our-projects/neworlean_tablet_354.jpg 354w, ./images/our-projects/neworlean_tablet_354x2.jpg  708w, ./images/our-projects/neworlean_desktop_370.jpg 370w, ./images/our-projects/neworlean_desktop_370x2.jpg 740w',
+      src: './images/our-projects/neworlean_tablet_354.jpg',
+      alt: 'постер з двома баскетболістами',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Постер New Orlean vs Golden Star',
+    itemText: 'Дизайн',
+  },
+
+  {
+    dataValue: 'apps',
+    image: {
+      srcset:
+        './images/our-projects/seafood_phone_450.jpg 450w, ./images/our-projects/seafood_phone_450x2.jpg 900w, ./images/our-projects/seafood_tablet_354.jpg 354w, ./images/our-projects/seafood_tablet_354x2.jpg  708w, ./images/our-projects/seafood_desktop_370.jpg 370w, ./images/our-projects/seafood_desktop_370x2.jpg 740w',
+      src: './images/our-projects/seafood_tablet_354.jpg',
+      alt: 'емблема ресторану у вигляді риби',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Ресторан Seafood',
+    itemText: 'Додаток',
+  },
+
+  {
+    dataValue: 'marketing',
+    image: {
+      srcset:
+        './images/our-projects/prime_phone_450.jpg 450w, ./images/our-projects/prime_phone_450x2.jpg 900w, ./images/our-projects/prime_tablet_354.jpg 354w, ./images/our-projects/prime_tablet_354x2.jpg  708w, ./images/our-projects/prime_desktop_370.jpg 370w, ./images/our-projects/prime_desktop_370x2.jpg 740w',
+      src: './images/our-projects/prime_tablet_354.jpg',
+      alt: 'навушники',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Проєкт Prime',
+    itemText: 'Маркетинг',
+  },
+
+  {
+    dataValue: 'apps',
+    image: {
+      srcset:
+        './images/our-projects/boxes_phone_450.jpg 450w, ./images/our-projects/boxes_phone_450x2.jpg 900w, ./images/our-projects/boxes_tablet_354.jpg 354w, ./images/our-projects/boxes_tablet_354x2.jpg  708w, ./images/our-projects/boxes_desktop_370.jpg 370w, ./images/our-projects/boxes_desktop_370x2.jpg 740w',
+      src: './images/our-projects/boxes_tablet_354.jpg',
+      alt: 'дві коробки',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Проєкт Boxes',
+    itemText: 'Додаток',
+  },
+
+  {
+    dataValue: 'web',
+    image: {
+      srcset:
+        './images/our-projects/inspiration_phone_450.jpg 450w, ./images/our-projects/inspiration_phone_450x2.jpg 900w, ./images/our-projects/inspiration_tablet_354.jpg 354w, ./images/our-projects/inspiration_tablet_354x2.jpg  708w, ./images/our-projects/inspiration_desktop_370.jpg 370w, ./images/our-projects/inspiration_desktop_370x2.jpg 740w',
+      src: './images/our-projects/inspiration_tablet_354.jpg',
+      alt: 'моноблок',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Inspiration has no Borders',
+    itemText: 'Веб-сайт',
+  },
+
+  {
+    dataValue: 'design',
+    image: {
+      srcset:
+        './images/our-projects/limited_phone_450.jpg 450w, ./images/our-projects/limited_phone_450x2.jpg 900w, ./images/our-projects/limited_tablet_354.jpg 354w, ./images/our-projects/limited_tablet_354x2.jpg  708w, ./images/our-projects/limited_desktop_370.jpg 370w, ./images/our-projects/limited_desktop_370x2.jpg 740w',
+      src: './images/our-projects/limited_tablet_354.jpg',
+      alt: 'розгорнутий журнал',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Видання Limited Edition',
+    itemText: 'Дизайн',
+  },
+
+  {
+    dataValue: 'marketing',
+    image: {
+      srcset:
+        './images/our-projects/lab_phone_450.jpg 450w, ./images/our-projects/lab_phone_450x2.jpg 900w, ./images/our-projects/lab_tablet_354.jpg 354w, ./images/our-projects/lab_tablet_354x2.jpg  708w, ./images/our-projects/lab_desktop_370.jpg 370w, ./images/our-projects/lab_desktop_370x2.jpg 740w',
+      src: './images/our-projects/lab_tablet_354.jpg',
+      alt: 'бірка на мотузці',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Проєкт LAB',
+    itemText: 'Маркетинг',
+  },
+
+  {
+    dataValue: 'apps',
+    image: {
+      srcset:
+        './images/our-projects/growing_phone_450.jpg 450w, ./images/our-projects/growing_phone_450x2.jpg 900w, ./images/our-projects/growing_tablet_354.jpg 354w, ./images/our-projects/growing_tablet_354x2.jpg  708w, ./images/our-projects/growing_desktop_370.jpg 370w, ./images/our-projects/growing_desktop_370x2.jpg 740w',
+
+      src: './images/our-projects/growing_tablet_354.jpg',
+      alt: 'людина працює за ноутбуком',
+    },
+    overlayText:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos quos magni deleniti earum cum soluta dolor culpa. Dolore, quos.',
+    itemTitle: 'Growing Business',
+    itemText: 'Додаток',
+  },
+];
